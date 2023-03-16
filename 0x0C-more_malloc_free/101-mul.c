fd62264c886bde506ff69e2f9385faf821c2e2c9
GL254 <kikwaig4@gmail.com>
2023-03-16 12:38:17 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,7 +20,7 @@ int is_digit(char *s)
 			return (0);
 		i++;
 	}
-
+}
 /**
  * _strlen - returns the len. of a string
  * @s: string to evaluate
@@ -37,7 +37,6 @@ int _strlen(char *s)
 	}
 	return (i);
 }
-
 /**
  * errors - handles errors for main function
  */
@@ -46,7 +45,6 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -58,6 +56,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -94,5 +93,6 @@ int main(int argc, char *argv[])
 		_putchar('0');
 	_putchar('\n');
 	free(result);
+
 	return (0);
 }
