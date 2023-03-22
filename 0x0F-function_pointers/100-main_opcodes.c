89e600a9577dadd0cc01569ca82f344ce81c844d
GL254 <kikwaig4@gmail.com>
2023-03-22 13:48:39 -0700

    0-print_name.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,7 +5,6 @@
  * main - prints its own opcodes
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -19,9 +18,7 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 
-
 		bytes = atoi(argv[1]);
-
 
 		if (bytes < 0)
 		{
@@ -29,9 +26,7 @@ int main(int argc, char *argv[])
 			exit(2);
 		}
 
-
 		arr = (char *)main;
-
 
 		for (i = 0; i < bytes; i++)
 		{
