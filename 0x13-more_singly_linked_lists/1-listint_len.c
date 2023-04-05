6fa8840b5711d2ff7c705c25bdcc36e877a6e456
GL254 <kikwaig4@gmail.com>
2023-04-05 09:28:44 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_n -  a function that returns the number of elements in a linked listint_t list.
+ * listint_n -  a function that returns the number of elements
+ * in a linked listint_t list.
  * @h: linked list of type listint_t to cross.
  *
  * Return: number of nodes
